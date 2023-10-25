0a66240dfcfe85502dd446044c51d87ebebd32e1
MiloMT <mylestaylor496@gmail.com>
2023-10-25 11:37:27 +1000

    Added flow for overwrite confirmation and encryption checking

--- __main__.py
+++ __main__.py
@@ -24,12 +24,25 @@ import os
 # import cryptography
 
 
-def pass_gen(args: object) -> list:
+def arg_check(
+    var: string, arg1: string, arg1_desc: string, arg2: string, arg2_desc: string
+) -> string:
+    while var not in (arg1, arg2):
+        var = input(
+            f"Sorry, but that wasn't a valid input. Please enter"
+            f"a [{arg1.upper()}] for {arg1_desc} or [{arg2.upper()}] "
+            f"for {arg2_desc}: ",
+        ).lower()
+
+    return var
+
+
+def pass_gen(args: object, regex: string = None) -> list:
     password = ""
     pass_list = []
     char_list = [string.ascii_lowercase]
 
-    # Flags to check for password character types
+    # Flags to check for password character types.
     if args.upper:
         char_list.append(string.ascii_uppercase)
     if args.number:
@@ -37,16 +50,16 @@ def pass_gen(args: object) -> list:
     if args.special:
         char_list.append(string.punctuation)
 
-    # Number of password to generate
+    # Number of password to generate.
     for _ in range(args.pass_num):
-        # Password generator dependent on char_num and arg flags
+        # Password generator dependent on char_num and arg flags.
         for _ in range(int(args.char_num)):
             password += random.choice(random.choice(char_list))
 
         pass_list.append(password)
         password = ""
 
-    # Copy passwords if flag entered
+    # Copy passwords if flag entered.
     if args.copy:
         pyperclip.copy("\n".join(pass_list))
 
@@ -57,36 +70,49 @@ def pass_gen(args: object) -> list:
     return pass_list
 
 
-def store_pass(pass_list: list) -> None:
-    # Check if passwords.txt already exists in current directory
+def store_pass(args: object, pass_list: list) -> bool:
+    # Check if passwords.txt already exists in current directory.
     if os.path.isfile("passwords.txt") == True:
-        # If it does, ask whether to overwrite or append passwords
+        # If it does, ask whether to overwrite or append passwords.
         print("There is already a passwords file in the current directory.")
-        write_confirm = input(
-            "Would you like to overwrite the file or append to",
+        write_type = input(
+            "Would you like to overwrite the file or append to "
             "this file? [W] or [A]: "
         )
-        while write_confirm not in ("w", "a"):
-            write_confirm = input(
-                "Sorry, but that wasn't a valid input. Please enter a [W]",
-                "for overwrite or [A] for append: ",
+        write_type = arg_check(write_type, "w", "overwrite", "a", "append")
+        # Ask user to confirm overwrite as long as ignore option wasn't used.
+        if write_type == "w" and args.force == False:
+            overwrite_confirm = input(
+                "This will overwrite your existing"
+                " password file, are you sure? [Y] or [N]: "
             ).lower()
-        with open("passwords.txt", write_confirm) as f:
-            if write_confirm == "a":
-                f.write("\n")
-            f.write("\n".join(pass_list))
+            overwrite_confirm = arg_check(overwrite_confirm, "y", "yes", "n", "no")
+
+        # Checks if force flag used or if user has confirmed overwrite.
+        if overwrite_confirm == "y" or args.force == True:
+            with open("passwords.txt", write_type) as f:
+                if write_type == "a":
+                    f.write("\n")
+                f.write("\n".join(pass_list))
+            # Bool value for whether passwords were saved or not
+            return True
+        else:
+            print("Save aborted")
+            # Bool value for whether passwords were saved or not
+            return False
     else:
-        # If not, say that file created
-        print("This file doesn't exist")
-        print(pass_list)
+        # If passwords.text doesn't exist, automatically create file.
         with open("passwords.txt", "w") as f:
             f.write("\n".join(pass_list))
+        print(
+            "Your passwords have been saved in the 'passwords.txt'",
+            "file in the current directory",
+        )
+        # Bool value for whether passwords were saved or not
+        return True
 
-    confirmation = input("Placeholder")
-    encrypt_pass()
 
-
-def encrypt_pass():
+def encrypt_pass(args: object, pass_list: list) -> None:
     # Ask whether to encrypt password txt file
     print("Testing")
     pass
@@ -97,21 +123,36 @@ def retrieve_pass():
     pass
 
 
+def regex_gen() -> string:
+    pass
+
+
 def main(args):
-    print("Passwords Generated:")
-    pass_list = pass_gen(args)
+    if args.regex == True:
+        regex = regex_gen()
+        print("Passwords Generated:")
+        pass_list = pass_gen(args, regex)
+    else: 
+        print("Passwords Generated:")
+        pass_list = pass_gen(args)
+        
     save_confirm = input(
         "Would you like to save your generated passwords? [Y] or [N]: "
     ).lower()
+    save_confirm = arg_check(save_confirm, "y", "yes", "n", "no")
 
     if save_confirm == "y":
-        store_pass(pass_list)
-    else:
-        while save_confirm not in ("y", "n"):
-            save_confirm = input(
-                "Sorry, but that wasn't a valid input. Please enter",
-                "a [Y] for yes or [N] for no: "
-            ).lower()
+        is_saved = store_pass(args, pass_list)
+
+    # Only asks to encrypt is passwords were saved
+    if is_saved == True:
+        encrypt_confirm = input(
+            "Would you like to encrypt your saved passwords? [Y] or [N]: "
+        ).lower()
+        encrypt_confirm = arg_check(encrypt_confirm, "y", "yes", "n", "no")
+
+        if encrypt_confirm == "y":
+            encrypt_pass(args, pass_list)
 
 
 if __name__ == "__main__":
@@ -151,6 +192,12 @@ if __name__ == "__main__":
         "-c",
         "--copy",
         help="Copies the password to the clipboard once generated",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-f",
+        "--force",
+        help="Forces overwrite so app ignores overwrite confirmation",
         action="store_true",
     )
     parser.add_argument(
